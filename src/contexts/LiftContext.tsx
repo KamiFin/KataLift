ad262b2ed8bbb6b3ab040c66e7fe3a6d3e559c2b
KamiFin <rigamonti.filippo@gmail.com>
2022-05-26 01:40:49 +0200

    First test

---
+++ src/contexts/LiftContext.tsx
@@ -0,0 +1,39 @@
+
+import { useContext, createContext, Dispatch } from "react";
+import { Lift, MovementSystem } from "../types/Lift";
+
+export const LiftContext = createContext<[MovementSystem, Dispatch<{ type: string; payload: any }>]>([
+  {floors: [], movements:[], requests: []},
+  () => null,
+]);
+
+export const liftReducers = (state: MovementSystem, actions: { type: string; payload: any }) => {
+  switch (actions.type) {
+    case "ADD_FLOOR": {
+        const cpyState: MovementSystem = {...state};
+        cpyState.floors = [...cpyState.floors,
+            actions.payload]
+      return cpyState;
+    }
+
+    case "ADD_MOVEMENT": {
+      const cpyState: MovementSystem = {...state};
+      cpyState.movements = [...cpyState.movements,
+          actions.payload]
+      return cpyState;
+    }
+
+  case "ADD_REQUEST": {
+    const cpyState: MovementSystem = {...state};
+    cpyState.requests = [...cpyState.requests,
+        actions.payload]
+    return cpyState;
+  }
+  }
+
+  return state;
+};
+
+export function useLiftInfo() {
+  return useContext(LiftContext);
+}
