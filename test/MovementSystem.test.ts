6a40f0cc5b47db6b8a148012a51c5768af042631
KamiFin <rigamonti.filippo@gmail.com>
2022-06-03 05:02:19 +0200

    tolto skip

--- test/MovementSystem.test.ts
+++ test/MovementSystem.test.ts
@@ -5,20 +5,20 @@ import { Directions } from '../src/constants/directions';
 import { movementSystemInAction, nearestLifts, reverseFloors } from '../src/utils';
 
 describe('LiftSystemPrinter', function () {
-  it.skip('No lifts', function () {
+  it('No lifts', function () {
       const floors: number[] = [0, 1, 2, 3];
       const movementSystem: MovementSystem = {floors, lifts: [], requests: []}
       verify(print(movementSystem, true));
   });
 
-  it.skip('Lift no doors', function () {
+  it('Lift no doors', function () {
       const lift: Lift = { id: "0", floorNumber: 0, floorRequest: [2, 3], isDoorsOpen: false };
       const floors: number[] = [0, 1, 2, 3];
       const movementSystem: MovementSystem = {floors, lifts: [lift], requests: []}
      verify(printWithoutDoors(movementSystem));
   });
 
-  it.skip('Movement system with 4 lifts', function () {
+  it('Movement system with 4 lifts', function () {
       const lift1: Lift = { id: "0", floorNumber: 3, floorRequest: [0], isDoorsOpen: false };
       const lift2: Lift ={ id: "2", floorNumber: 2,  floorRequest: [], isDoorsOpen: true};
       const lift3 : Lift ={ id: "3", floorNumber: 2, floorRequest: [], isDoorsOpen: false };
